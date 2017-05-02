d5ca8c6750a9b993c43e61285ea218004ac060f6
jonotko <j.mastermind1010@gmail.com>
2017-05-02 21:03:19 +0000

    Final

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
+var ip = require("ip");
 
 var app = express();
+var useragent = require('express-useragent');
+
+app.use(useragent.express());
 
 app.get("/", function(req, res){
    res.send("Jonathan's Req Header Parser Microservice "); 
@@ -8,7 +12,17 @@ app.get("/", function(req, res){
 
 
 app.get("/whoami", function(req, res) {
-   res.send("you'll soon find out who you are"); 
+   var ipAddres =  req.ip;
+   var os =  req.useragent.os;
+   var language = req.headers["accept-language"].split(",")[0];
+   
+   var object = {
+       ipaddress: ipAddres,
+       language: language,
+       software: os
+   }
+    
+   res.send(object); 
 });
 
 app.listen(process.env.PORT, function(){
